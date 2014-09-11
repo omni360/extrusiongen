ff32c8f2831122d511adbb81f2f0f7cb42c32b47
Ikaros Kappler <ikaros.kappler@gmx.de>
2014-09-11 17:41:03 +0200

    Added 'bend' param when loading from the gallery.

--- function.isDildoGeneratorDomain.js
+++ function.isDildoGeneratorDomain.js
@@ -10,6 +10,7 @@
 
 function isDildoGeneratorDomain() {
     return ( window.location.host.toLowerCase().indexOf("dildo-generator.com") != -1 ||
-             window.location.host.toLowerCase().indexOf("dildogenerator.com") != -1
+             window.location.host.toLowerCase().indexOf("dildogenerator.com") != -1 ||
+	     window.location.host.toLowerCase().indexOf("wenn-der-dödel-drei-mal-rödelt-dann.com") != -1
            );    
 }
